61bfee53088a085b19b187cbb97bbf04f80091c1
Tautvydas Vaiciunas <vaiciunast@gmail.com>
2025-10-02 12:32:23 +0300

    Import AuthModule into UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
 import { UsersController } from './users.controller';
 import { User } from './user.entity';
 import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), AuthModule],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
   exports: [UsersService, UsersRepository]
